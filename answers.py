489a78990ac24a9db4ab88c810f67b4249764791
Gangster <40878621+GangsterGpc@users.noreply.github.com>
2025-01-09 17:42:46 +0630

    9.1.2025 update answers.py

--- answers.py
+++ answers.py
@@ -1,45 +1,45 @@
 
 def get_price_prediction_answer(card_id):
     answers = {
-    "9066": "13000",
-    "9063": "33000",
-    "9062": "46000",
-    "9067": "438000",
-    "9065": "21000",
-    "9064": "13000",
-    "9071": "12000",
-    "9069": "567000",
-    "9068": "19000",
-    "9070": "30000"
+    "9121": "34000",
+    "9112": "60000",
+    "9115": "48000",
+    "9113": "100000",
+    "9114": "13000",
+    "9117": "11000",
+    "9116": "26000",
+    "9119": "23000",
+    "9118": "14000",
+    "9120": "12000"
 }
     return answers.get(str(card_id), None)
 
 def get_who_is_the_artist_answer(card_id):
     answers = {
-    "9052": "Pu Ru",
-    "9053": "Qi Baishi",
-    "9059": "George Condo",
-    "9055": "Alex Katz",
-    "9058": "Anish Kapoor",
-    "9054": "Damien Hirst",
-    "9061": "Jeff Koons",
-    "9056": "Peter Doig",
-    "9057": "Kaws",
-    "9060": "Gerhard Richter"
+    "9102": "Qi Baishi",
+    "9103": "Lin Fengmian",
+    "9105": "Damien Hirst",
+    "9104": "Pu Ru",
+    "9106": "Alex Katz",
+    "9107": "Takashi Murakami",
+    "9108": "Gerhard Richter",
+    "9110": "Richard Prince",
+    "9109": "Jeff Koons",
+    "9111": "Kaws"
 }
     return answers.get(str(card_id), None)
 
 def get_artist_market_value_answer(card_id):
     answers = {
-    "9044": "$460.1 million",
-    "9042": "$851.3 million",
-    "9043": "$205.6 million",
-    "9045": "$545.1 million",
-    "9046": "$252.8 million",
-    "9047": "$2.1 billion",
-    "9049": "$254.1 million",
-    "9051": "$603.8 million",
-    "9048": "$203.8 million",
-    "9050": "$505.2 million"
+    "9092": "$851.3 million",
+    "9093": "$205.6 million",
+    "9094": "$552.6 million",
+    "9095": "$460.1 million",
+    "9096": "$96.5 million",
+    "9097": "$545.1 million",
+    "9099": "$254.1 million",
+    "9098": "$252.8 million",
+    "9100": "$177.3 million",
+    "9101": "$937.8 million"
 }
     return answers.get(str(card_id), None)
