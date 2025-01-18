a7a1c5897d6762e4c6c88cfb6fdded260ca6a907
Gangster <40878621+GangsterGpc@users.noreply.github.com>
2025-01-18 18:48:47 +0630

    18.1.2025 update answers.py

--- answers.py
+++ answers.py
@@ -1,45 +1,45 @@
 
 def get_price_prediction_answer(card_id):
     answers = {
-    "9513": "29000",
-    "9512": "28000",
-    "9514": "538000",
-    "9517": "120000",
-    "9515": "33000",
-    "9516": "92000",
-    "9520": "342000",
-    "9518": "166000",
-    "9519": "12000",
-    "9521": "61000"
+    "9563": "31000",
+    "9568": "50000",
+    "9562": "28000",
+    "9565": "23000",
+    "9564": "177000",
+    "9566": "45000",
+    "9567": "63000",
+    "9569": "151000",
+    "9571": "332000",
+    "9570": "266000"
 }
     return answers.get(str(card_id), None)
 
 def get_who_is_the_artist_answer(card_id):
     answers = {
-    "9511": "Huang Zhou",
-    "9502": "Qi Gong",
-    "9503": "Henri Matisse",
-    "9504": "Lee Ufan",
-    "9505": "Zhang Daqian",
-    "9508": "Joan Mitchell",
-    "9507": "Wassily Kandinsky",
-    "9506": "David Hockney",
-    "9510": "Sanyu",
-    "9509": "Guan Liang"
+    "9557": "Henry Moore",
+    "9553": "Guan Liang",
+    "9554": "Camille Pissarro",
+    "9552": "Edgar Degas",
+    "9555": "Yoshitomo Nara",
+    "9556": "Sean Scully",
+    "9561": "Zhang Daqian",
+    "9558": "Pierre-Auguste Renoir",
+    "9559": "Yves Klein",
+    "9560": "Banksy"
 }
     return answers.get(str(card_id), None)
 
 def get_artist_market_value_answer(card_id):
     answers = {
-    "9492": "$205.6 million",
-    "9494": "$385.5 million",
-    "9493": "$49.0 million",
-    "9496": "$13.0 billion",
-    "9495": "$545.1 million",
-    "9497": "$107.7 million",
-    "9498": "$1.0 billion",
-    "9500": "$2.1 billion",
-    "9499": "$350.2 million",
-    "9501": "$2.4 billion"
+    "9542": "$3.2 billion",
+    "9543": "$5.3 billion",
+    "9544": "$203.8 million",
+    "9545": "$276.5 million",
+    "9546": "$561.8 million",
+    "9548": "$2.7 billion",
+    "9549": "$1.3 billion",
+    "9551": "$1.8 billion",
+    "9547": "$851.3 million",
+    "9550": "$3.1 billion"
 }
     return answers.get(str(card_id), None)
